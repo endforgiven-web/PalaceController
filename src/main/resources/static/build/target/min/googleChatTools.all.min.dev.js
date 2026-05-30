3e086f6ff6b1a323a374287c1a76454a3f25b0c7
goodgrammer1815@outlook.com <goodgrammer1815@outlook.com>
2026-05-30 18:22:17 -0500

    removed extra spacing in gcutils and converted or built Main as a typescript file.

--- src/main/resources/static/build/target/min/googleChatTools.all.min.dev.js
+++ src/main/resources/static/build/target/min/googleChatTools.all.min.dev.js
@@ -19,31 +19,54 @@ let hasCycleRun = false;
 
 "use strict";
 class GCUtils {
-    static getSelectedChat() {
+    static SCRAPE(x) {
+        GCUtils.goToXChat(x);
+        setTimeout(() => {
+            const convName = GCUtils.GET_CURR_CONV_NAME();
+            const convMessages = GCUtils.GET_CURR_CONV_MESSAGES_TEXT();
+            const formattedMessages = GCUtils.FORMAT_CONV_MESSAGES(convMessages);
+            console.log(`Conversation Name: ${convName}`);
+            console.log(`Conversation Messages: ${formattedMessages}`);
+        }, 10000);
+    }
+    static GET_SELECTED_CHAT() {
         return document.querySelector('.IL9EXe.PL5Wwe.dHI9xe.rcdhB.WD3P7.qs41qe');
         ;
     }
-    static getAllChats() {
+    static GET_ALL_CHATS() {
         return document.querySelectorAll('.IL9EXe.PL5Wwe.dHI9xe.rcdhB');
     }
     static goToXChat(x) {
-        GCUtils.getAllChats()[x].click();
-        return GCUtils.getSelectedChat().textContent;
+        GCUtils.GET_ALL_CHATS()[x].click();
+        return GCUtils.GET_SELECTED_CHAT().textContent;
     }
-    static getCurrConvName() {
+    static GET_CURR_CONV_NAME() {
         const res = document.querySelectorAll('.tB5Jxf-xl07Ob-XxIAqe-OWXEXe-oYxtQd.TZQ8c.UXqkkf.Da1OLc .njhDLd.O5OMdc')[0];
         return res != undefined ? res.textContent : "NoName";
     }
-    static getCurrConvMessages() {
+    static GET_CURR_CONV_MESSAGES() {
         const res = document.querySelectorAll('.auHzcc.cFc9ae')[0];
         return res != undefined ? res : [];
     }
-    static getCurrConvMessagesText() {
-        return GCUtils.getCurrConvMessages().textContent;
+    static GET_CURR_CONV_MESSAGES_TEXT() {
+        return GCUtils.GET_CURR_CONV_MESSAGES().textContent;
     }
-    static formatConvMessages(messages = GCUtils.getCurrConvMessages()) {
+    static FORMAT_CONV_MESSAGES(messages = GCUtils.GET_CURR_CONV_MESSAGES()) {
         return messages.replaceAll('You', '/nYou').replaceAll('Add reaction', '/n');
     }
+}
+
+"use strict";
+(function () {
+    'use strict';
+    console.log('gctools');
+    // Your code here...
+    setTimeout(() => {
+        GCUtils.SCRAPE(3);
+    }, 1500);
+})();
+function test() {
+    console.log("test");
 }
 
 "use strict";
---
+++ src/main/resources/static/js/GoogleChatTools/Main.ts
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+    console.log('gctools');
+    // Your code here...
+    setTimeout(()=>{
+        GCUtils.SCRAPE(3);
+    }, 1500);
+})();
+
+function test() {
+    console.log("test");
+}
